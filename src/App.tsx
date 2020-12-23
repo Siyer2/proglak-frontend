bc6eb7f275eb5a41b2df8bc2dc284c91f74365c4
Syam Iyer <iyersyam@gmail.com>
2020-12-24 10:41:30 +1100

    Added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import './bootstrap.min.css';
 import './App.css';
+
+import { OverlayTrigger, Button, Popover } from 'react-bootstrap';
+
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      {/* <Main /> */}
+      <footer style={{ textAlign: 'left', paddingLeft: '5px' }}>
+        <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+          <Button variant="success">New Features 🎉</Button>
+        </OverlayTrigger>
+      </footer>
+      <footer style={{ textAlign: 'right' }}>
+        <p>By <a href="https://github.com/Siyer2" target="_blank" rel="noopener noreferrer">Syam</a>&nbsp;&nbsp;</p>
+      </footer>
     </div>
   );
 }
+
+const newFeatures = [
+  "Double Majors"
+]
+const popover = (
+  <Popover id="popover-basic">
+    <Popover.Title as="h3">11th December, 2020</Popover.Title>
+    {newFeatures.map((feature, i) => {
+      return (
+        <Popover.Content key={i + feature}>
+          + {feature}
+        </Popover.Content>)
+    })}
+  </Popover>
+);
 
 export default App;
