3115d0501d8e38ae03877c8a285daaf11bc27af5
Syam Iyer <iyersyam@gmail.com>
2020-12-25 10:06:22 +1100

    Moved browser router to index

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import ProgramSelector from './components/ProgramSelector';
 import Results from './components/Results';
 
 const Main = () => {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route exact path={'/'} component={ProgramSelector}></Route>
-                <Route exact path={'/results'} component={Results}></Route>
-            </Switch>
-        </BrowserRouter>
+        <Switch>
+            <Route exact path={'/'} component={ProgramSelector}></Route>
+            <Route exact path={'/results'} component={Results}></Route>
+        </Switch>
     )
 }
 
